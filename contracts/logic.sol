c05dbbd4546a7e0266eb75933ceb23582e7399b0
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-05 15:38:59 +0530

    Update logic.sol

--- contracts/logic.sol
+++ contracts/logic.sol
@@ -2,20 +2,48 @@ pragma solidity ^0.5.12;
 
 contract milkyChain{
 
-address public weigher;
-address public tester;
-address public chiller;
-address public pasteurizer;
-address public packer;
-address public dispatcher;
+    address public weigher;
+    address public tester;
+    address public chiller;
+    address public pasteurizer;
+    address public packer;
+    address public dispatcher;
 
 
-struct milkIn{
+  struct milkIn{
     string from;
     uint quantity;
     string vehicle;
     uint date;
-     }
+    }
 
+
+
+  struct milkTest{
+   uint acidity;
+   uint alchol;
+   uint fat;
+   uint clr;
+   uint sugar;
+   uint neutraizer;
+   uint salt;
+   uint urea;
+
+  }
+
+  struct pasteurizer {
+    
+    uint temparature;
+    uint waterQuantity;
+   }
+
+  struct LorryDispatch{
+   
+   uint milkQuantity;
+   uint totalTrays;
+   uint emptyTrays;
+   uint returnedTrays;
+  }
 
 }
+
