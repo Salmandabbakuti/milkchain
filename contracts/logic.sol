8f9d9e0874b5a083b4c714e2ae7b5667c83ba74e
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-05 15:16:21 +0530

    Update logic.sol

--- contracts/logic.sol
+++ contracts/logic.sol
@@ -1,1 +1,21 @@
+pragma solidity ^0.5.12;
 
+contract milkyChain{
+
+address public weigher;
+address public tester;
+address public chiller;
+address public pasteurizer;
+address public packer;
+address public dispatcher;
+
+
+struct milkIn{
+    string from;
+    uint quantity;
+    string vehicle;
+    uint date;
+     }
+
+
+}
